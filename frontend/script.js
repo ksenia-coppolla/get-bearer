e3deb77997658ad93d0191a59cf9f2dcd4982b79
Ksenia C <koppolla@gmail.com>
2024-05-26 11:45:53 +0200

    chore: fix routes

--- frontend/script.js
+++ frontend/script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ).value;
 
     try {
-      const response = await fetch(`/backend/api/generate-token.js`, {
+      const response = await fetch(`/api/generate-token`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
